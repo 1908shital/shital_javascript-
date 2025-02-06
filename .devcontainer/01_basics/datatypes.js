420e0e835a8e19725cf7a0d3f021a642d8165be2
1908shital <143158155+1908shital@users.noreply.github.com>
2025-02-06 11:50:33 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert(3+3) // using node.js,not browser
+
+console.log(3+3);
+console.log("shital");
+
+let name= "shital"
+let age = 20
+let isLoggedIn = false
+
+let state;
+
+console.log(typeof "shital");
+
+
